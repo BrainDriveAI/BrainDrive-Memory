002e68c36bdb9cc38e81c77637bdf7b1d5b73bd7
Beck Bekmyradov <47065940+bekmuradov@users.noreply.github.com>
2025-05-30 19:48:45 +0400

    Udpate create react agent params

--- app/agents/react_graph_agent.py
+++ app/agents/react_graph_agent.py
@@ -1,10 +1,9 @@
 from langgraph.prebuilt import create_react_agent
-from langchain.schema import SystemMessage
 
 from app.adapters.llm_adapter import search_llm_provider
 from app.agent_prompts.system_prompt_manager import SystemPromptManager
 
-from app.tools.search.search_tool import search_for_memories
+from app.tools.search.new_search_tool import search_for_memories
 from app.tools.add.add_tool import add_graph_memory
 from app.tools.get_all.get_all_tool import get_all_items
 from app.tools.update.update_tool import update_graph_memory
@@ -24,7 +23,11 @@ tools = [
 # Get the appropriate system prompt
 agent_system_prompt = SystemPromptManager.get_system_prompt()
 
-graph = create_react_agent(search_llm_provider, tools=tools, state_modifier=SystemMessage(content=agent_system_prompt))
+graph = create_react_agent(
+    search_llm_provider,
+    tools=tools,
+    prompt=agent_system_prompt,
+)
 
 
 def invoke_our_graph(st_messages, callables):
