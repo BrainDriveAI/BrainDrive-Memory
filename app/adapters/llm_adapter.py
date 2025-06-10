e3b1cdd311bdf605ec34144ea0da53c952563126
Beck Bekmyradov <47065940+bekmuradov@users.noreply.github.com>
2025-06-10 19:56:06 +0400

    Update llm providers

--- app/adapters/llm_adapter.py
+++ app/adapters/llm_adapter.py
@@ -1,6 +1,11 @@
 from app.config.app_env import app_env
 
 def get_llm():
+    """
+    Factory function to provide the appropriate LLM for chat-based operations.
+    This will be used by LangGraph's create_react_agent.
+    """
+
     provider = app_env.LLM_PROVIDER
 
     if provider == 'openai':
@@ -12,13 +17,53 @@ def get_llm():
             verbose=False,
             streaming=True
         )
+    
+    if provider == 'openrouter':
+        from langchain_openai import ChatOpenAI
+        return ChatOpenAI(
+            temperature=0,
+            model="meta-llama/llama-3.3-8b-instruct:free",
+            api_key=app_env.OPENROUTER_API_KEY.get_secret_value(),
+            base_url=str(app_env.OPENROUTER_BASE_URL),
+            verbose=False,
+            streaming=True
+        )
 
     if provider == 'ollama':
         from langchain_ollama import ChatOllama
         return ChatOllama(
             model=app_env.OLLAMA_LLM_MODEL,
-            base_url=str(app_env.OLLAMA_BASE_URL) if app_env.OLLAMA_BASE_URL else None
+            base_url=str(app_env.OLLAMA_BASE_URL) if app_env.OLLAMA_BASE_URL else None,
+            temperature=0,
+            verbose=False,
+            streaming=True
         )
+    
+    # if provider == 'cloud_run_gemma':
+    #     from app.adapters.custom_ollama_cloud_run import create_cloudrun_gemma_llm
+    #     service_url = str(app_env.GEMMA_SERVICE_URL)
+    #     # Remove any trailing slashes from service URL
+    #     service_url = service_url.rstrip('/')
+    #     return create_cloudrun_gemma_llm(
+    #         service_url=service_url,
+    #         api_key=app_env.GEMMA_API_KEY.get_secret_value(),
+    #         service_account_path=app_env.GEMMA_SERVICE_ACCOUNT_PATH,
+    #         model_name="gemma3:27b",
+    #         temperature=0.7,
+    #         max_tokens=1000
+    #     )
+    if provider == 'cloud_run_gemma':
+        from langchain_openai import ChatOpenAI
+        return ChatOpenAI(
+            temperature=0,
+            model=app_env.GEMMA_LLM_MODEL,
+            api_key=app_env.GEMMA_API_KEY.get_secret_value(),
+            base_url=str(app_env.GEMMA_SERVICE_URL),
+            openai_api_key=app_env.GEMMA_API_KEY.get_secret_value(),
+            verbose=False,
+            streaming=True
+        )
+
 
     if provider == 'togetherai':
         from langchain_together import ChatTogether
